264bd66e1042ff093adf35b997c6f8ea2b3fc27f
Emelie Sannerfelt <emelie.sannerfelt@hotmail.com>
2021-03-26 11:33:25 +0100

    Added more calculators, dont know if this is right

--- src/main/java/com/calculator/maven/_pass_emelieSannerfeltSaraChristensen/Main.java
+++ src/main/java/com/calculator/maven/_pass_emelieSannerfeltSaraChristensen/Main.java
@@ -3,8 +3,8 @@ package com.calculator.maven._pass_emelieSannerfeltSaraChristensen;
 public class Main {
 
 	public static void main(String[] args) {
-		BasicCalculator basicCalculator = new BasicCalculator();
 		
+		BasicCalculator basicCalculator = new BasicCalculator();
 		System.out.println(basicCalculator.addition(4.6, 6.5));
 		
 		System.out.println(basicCalculator.subtraction(44.6, 6.5));
@@ -17,6 +17,12 @@ public class Main {
 		System.out.println(advancedCalculator.square(10.0));
 		
 		System.out.println(advancedCalculator.cube(10));//ett tal gånger 3
+		
+		System.out.println(advancedCalculator.squareroot(Math.sqrt(9)));
+		
+		System.out.println(advancedCalculator.absoluteValue(Math.abs(-3)));
+		
+		System.out.println(advancedCalculator.raisedToThePowerOf(1, 6));
 
 	}
 
