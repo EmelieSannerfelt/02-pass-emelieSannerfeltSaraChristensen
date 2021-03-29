ba76888b672195736397dd5c20199bbc77dd1672
Emelie Sannerfelt <emelie.sannerfelt@hotmail.com>
2021-03-29 10:45:54 +0200

    added more tests

--- src/test/java/com/calculator/maven/_pass_emelieSannerfeltSaraChristensen/AdvancedCalculatorTest.java
+++ src/test/java/com/calculator/maven/_pass_emelieSannerfeltSaraChristensen/AdvancedCalculatorTest.java
@@ -5,10 +5,15 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class AdvancedCalculatorTest {
+	AdvancedCalculator advancedCalculator = new AdvancedCalculator();
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertEquals(advancedCalculator.cube(4),64,0);
+		assertEquals(advancedCalculator.square(10),100,0);
+		assertEquals(advancedCalculator.squareroot(9),1,73);
+		assertEquals(advancedCalculator.absoluteValue(-3),3,0);
+		assertEquals(advancedCalculator.raisedToThePowerOf(1,6),1,0);
 	}
 
 }
